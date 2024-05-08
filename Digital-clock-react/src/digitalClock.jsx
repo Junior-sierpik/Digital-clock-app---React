7def3d3a34098e7525be4f86c5a52b79206ba796
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-08 16:51:05 +0200

    Finish project

--- Digital-clock-react/src/digitalClock.jsx
+++ Digital-clock-react/src/digitalClock.jsx
@@ -22,7 +22,11 @@ function digitalClock(){
 
     hours = hours % 12 || 12;
 
-    return `${hours}:${minutes}:${seconds} ${meridiem}`;
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+  }
+
+  function padZero(number) {
+    return (number < 10 ? "0" : "") + number;
   }
 
   return(
