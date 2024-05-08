65da60c108a2482c86c1dea3ce603b4c7ebdebea
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-08 16:34:33 +0200

    HTML and background image added now.

--- Digital-clock-react/src/digitalClock.jsx
+++ Digital-clock-react/src/digitalClock.jsx
@@ -1,13 +1,15 @@
-
+import React, {useState, useEffect} from "react";
 
 function digitalClock(){
 
 
 
   return(
-    <>
-    
-    </>
+    <div className="clock-container">
+      <div className="clock">
+          <span>00:00:00</span>
+      </div>
+    </div>
   );
 }
 
