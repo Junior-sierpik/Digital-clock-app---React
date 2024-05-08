fb54861b47258e360b36f7d5d2847f78d1cc5f6c
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-08 16:47:49 +0200

    Create formatTime function and export it to HTML

--- Digital-clock-react/src/digitalClock.jsx
+++ Digital-clock-react/src/digitalClock.jsx
@@ -5,13 +5,30 @@ function digitalClock(){
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return() => {
+      clearInterval(intervalId);
+    }
   }, []);
+
+  function formatTime() {
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    return `${hours}:${minutes}:${seconds} ${meridiem}`;
+  }
 
   return(
     <div className="clock-container">
       <div className="clock">
-          <span>00:00:00</span>
+          <span>{formatTime()}</span>
       </div>
     </div>
   );
